fceb1aac3bd3c2ad40bf709f51bd32293259ce2b
DemeterCaubang <dpcaubang@student.apc.edu.ph>
2022-04-12 22:54:08 +0800

    links changed, cordova enabled to change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'kalinga-at-yakap-app',
+  appName: 'Kalinga At Yakap App',
   webDir: 'www',
   bundledWebRuntime: false
 };
