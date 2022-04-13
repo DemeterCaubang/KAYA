01b6839a49077a43d62fa3f45564d7962d99799a
DemeterCaubang <dpcaubang@student.apc.edu.ph>
2022-04-14 04:10:47 +0800

    logos added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public communityIcon = '../assets/custom_icons/Community.png';
+  private communityActive = '../assets/custom_icons/Community-Colored.png';
+  public consultationIcon = '../assets/custom_icons/Consultation.png';
+  public healthAndWellnessIcon = '../assets/custom_icons/Health-and-Wellness.png';
+  public networkIcon = '../assets/custom_icons/Diary.png';
+  public webinarsIcon = '../assets/custom_icons/Webinars.png';
 
   constructor() {}
 }
